ec4f392a27f33b72fb51e929140794e5e608cf21
TJCyan <47479486+tjin2019@users.noreply.github.com>
2024-11-22 11:55:13 -0500

    The main viral code programs

---
+++ blackmail.js
@@ -0,0 +1,3 @@
+console.log("NOTE: THIS DOESN'T ACTUALLY IMPORT A VIRUS THIS IS JUST AN EXAMPLE PLEASE DON'T SUE US")
+console.log("If you don't share the extension link with 10 people, we will import a virus real real trust trust")
+alert("CHECK CONSOLE LOGS (use Alt-Brightness [the fourth function key])")
